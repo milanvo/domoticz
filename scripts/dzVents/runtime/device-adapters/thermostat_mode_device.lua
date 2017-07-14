ad827ed7999dfdb82f4176d46205616d369451ef
milanvo <milanvo@users.noreply.github.com>
2017-07-14 13:02:32 +0200

    dZvents device adapters: thermostat_mode_device, thermostat_setpoint_virtual - cleanup of comments

--- scripts/dzVents/runtime/device-adapters/thermostat_mode_device.lua
+++ scripts/dzVents/runtime/device-adapters/thermostat_mode_device.lua
@@ -14,14 +14,7 @@ return {
 
 	process = function (device, data, domoticz, utils, adapterManager)
 
-		-- device['SetPoint'] = device.rawData[1] or 0
-
 		function device.updateMode(mode)
-			-- send the command using openURL otherwise, due to a bug in Domoticz, you will get a timeout on the script
-
-			-- local url = domoticz.settings['Domoticz url']..
-			-- 		'/json.htm?type=command&param=udevice&idx=' .. device.id .. '&nvalue=' .. mode
-			-- utils.log('Setting mode using openURL ' .. url, utils.LOG_DEBUG)
 			device.update(mode, mode)
 		end
 
--- scripts/dzVents/runtime/device-adapters/thermostat_setpoint_virtual.lua
+++ scripts/dzVents/runtime/device-adapters/thermostat_setpoint_virtual.lua
@@ -18,9 +18,6 @@ return {
 
 		function device.updateSetPoint(setPoint)
 			-- send the command using openURL otherwise, due to a bug in Domoticz, you will get a timeout on the script
-
-			-- local url = domoticz.settings['Domoticz url']..
-			--		'/json.htm?type=command&param=udevice&idx=' .. device.id .. '&nvalue=0&svalue=' .. setPoint
 			local url = domoticz.settings['Domoticz url']..
 					'/json.htm?type=command&param=setsetpoint&idx=' .. device.id .. '&setpoint=' .. setPoint
 			utils.log('Setting setpoint using openURL ' .. url, utils.LOG_DEBUG)
